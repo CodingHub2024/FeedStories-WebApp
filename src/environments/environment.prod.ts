2e7e5af062fefe49b31b21732ea863a9f2e54924
Anurag Bhagatsing Solanke <anuragsolanke326@gmail.com>
2024-08-11 15:52:28 +0530

    changed environment.prod.ts file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://feedstorieswebapp-csbfdbgdc4bggfbg.northeurope-01.azurewebsites.net/",
+  apiUrl: "https://feedstorieswebapi-a0bqgsfeffavhvch.northeurope-01.azurewebsites.net/",
   feedStories: {
     pageSize: 20, // Page size configuration
   },
